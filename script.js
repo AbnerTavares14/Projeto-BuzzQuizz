c3322f5636d705d7f16dc0be5325b39c1a894402
Abner <abnerdasilvatavares@gmail.com>
2022-02-16 13:09:05 -0300

    Botão CriarQuizz e tela inicial da criação quiz com as validações dos inputs

--- script.js
+++ script.js
@@ -25,4 +25,29 @@ function listarTodosQuizzes(){
 
 listarTodosQuizzes()
 
+function criarQuiz(){
+    window.location.href = "criacaoQuiz.html";
+}
 
+function irCriarPerguntas(){
+    let re = "^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?";
+    const titulo = document.querySelector(".titulo1").value;
+    const url = document.querySelector(".url1").value;
+    const perguntas = document.querySelector(".perguntas1").value;
+    const niveis = document.querySelector(".niveis1").value;
+    let condicoes = 0;
+    if((titulo.length >= 20 && titulo.length <= 65) && (niveis >= 2) && (perguntas >= 3)){
+        window.location.href = "https://www.google.com/"; 
+    }else{
+        alert("Por favor, preencha os dados corretamente!");
+        let limpaTitulo = document.querySelector(".titulo1");
+        limpaTitulo.value = "";
+        let limpaURL = document.querySelector(".url1");
+        limpaURL.value = "";
+        let limpaPerguntas = document.querySelector(".perguntas1");
+        limpaPerguntas.value = "";
+        let limpaNiveis = document.querySelector(".niveis1");
+        limpaNiveis.value = "";
+        window.location.reload;
+    }
+}
