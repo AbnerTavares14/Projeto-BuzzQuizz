c82c9775eb183f7683540f8cfe14d76631b4328a
Carlos Gadelha <josecarlos.gadelha10@gmail.com>
2022-02-18 23:04:07 -0300

    validação da tela 3.3 niveis

--- script.js
+++ script.js
@@ -171,6 +171,7 @@ function validaInformacoesBasicas() {
         limpaPerguntas.value = "";
         let limpaNiveis = document.querySelector(".niveis1");
         limpaNiveis.value = "";
+
         criarPerguntas();
     } else { //Caso seja falso, é exibido um alert e os inputs são limpos e a página recarregada.
         alert("Por favor, preencha os dados corretamente!");
@@ -329,6 +330,51 @@ function verificaColor(elemento){
     return true;
 }
 
+function verificarURL(tela){
+
+    const urlCorreta = [...document.querySelectorAll(`.${tela} .url-correta`)]
+
+    let controlador = 0
+    regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+
+
+    for (let i = 0; i < urlCorreta.length; i++) {
+
+        if(regexp.test(urlCorreta[i].value) === false){
+            controlador++
+            break;
+        }
+
+    }
+
+    return (controlador !== 0) ? false : true
+}
+
+function verificarPorcentagem(){
+
+        
+    const porcentagem = [...document.querySelectorAll(`.tela3-niveis .porcentagem`)]
+
+    let nivelZero = 0
+    let controlador = 0
+
+    for (let i = 0; i < porcentagem.length; i++) {
+
+        if(parseInt(porcentagem[i].value) === 0){
+            nivelZero++
+        }else{
+            if((parseInt(porcentagem[i].value) >= 0 &&  parseInt(porcentagem[i].value) <=100)){
+               controlador++
+            }
+        }
+
+    }
+
+    
+    return (nivelZero !== 0 && controlador !== 0) ? true : false
+}
+
+
 function criarNiveis(){
     const pergunta = document.querySelector(".tela3-perguntas"); //tela 3.1 onde cria as perguntas
     const niveis = document.querySelector(".tela3-niveis"); //tela 3.1 onde cria as perguntas
@@ -336,5 +382,61 @@ function criarNiveis(){
     remocao.classList.add("escondido"); //Some com a tela 3 inicial
     pergunta.classList.add("escondido"); //Some com a tela 3 inicial
     niveis.classList.remove("escondido"); //Faz aparecer a tela 3.1 onde cria as perguntas
+
+    for (let i = 0; i < qtdNiveis - 1; i++) {
+        niveis.innerHTML += `<div class="selecione">
+        <h2>Nivel ${i + 2}</h2>
+        <ion-icon name="create-outline" onclick="apareceCriacaoDaPergunta(this, 'p${i + 2}')"></ion-icon>
+    </div>
+    <div class="cria-niveis p${i + 2} escondido">
+
+        <input type="textarea" class="nivel" placeholder="Titulo do Nivel">
+        <input type="textarea" class="porcentagem" placeholder="% de acerto mínima">
+        <input type="url" class="url-correta" placeholder="URL da imagem do nível">
+        <input type="textarea" class="descricao" placeholder="Descrição do nível">
+
+    </div>`
+    }
+    //Após o fim do laço, adiciono o botão dinamicamente no final da página
+    niveis.innerHTML += `<button class="p-perguntas" onclick="validaInformacoesNiveis()"><p> Finalizar Quizz </p></button>`;
     
+}
+
+function validaInformacoesNiveis(){
+    let controlador = 0
+
+    const nivel = [...document.querySelectorAll(".nivel")];
+    const porcentagem = [...document.querySelectorAll(".porcentagem")];
+    const urlCorreta = [...document.querySelectorAll(".url-correta")];
+    const descricao = [...document.querySelectorAll(".descricao")];
+
+    for (let i = 0; i < nivel.length; i++) {
+
+        const verificarTituloNivel = nivel[i].value.length < 10
+        const verificarDescricao = descricao[i].value.length < 30
+        const verificarIntervaloPorcetagem = !verificarPorcentagem()
+        const verificarUrl = !verificarURL('tela3-niveis')
+
+        if (verificarTituloNivel || verificarDescricao|| verificarIntervaloPorcetagem || verificarUrl) {
+            controlador++
+            break;
+        }
+    }
+
+    if(controlador !== 0){
+        alert("Por favor preencha os dados corretamente!");
+        // limparCampos(pergunta, respostaCorreta, respostaIncorreta, urlCorreta, color)
+
+    } else {
+        criarSucessoQuizz()
+    }
+
+}
+
+function criarSucessoQuizz(){
+    const sucessoQuizz = document.querySelector(".tela3-sucessoQuizz"); //tela 3.1 onde cria as perguntas
+    const niveis = document.querySelector(".tela3-niveis"); //tela 3.1 onde cria as perguntas
+    niveis.classList.add("escondido"); //Some com a tela 3 inicial
+    sucessoQuizz.classList.remove("escondido"); //Faz aparecer a tela 3.1 onde cria as perguntas
+
 }
